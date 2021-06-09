a2b7b6d84b5ec1a1405aba7997400e03976be33a
StasUzumaki <stasl77775@gmail.com>
2021-06-09 10:40:38 +0300

    test text commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Fcomponent from './Fcomponent';
+import Classcomponent from './Classcomponent';
+import Menu from './Menu';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Fcomponent />
+        <Classcomponent />
+        <Menu />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
          <h1>Hello Den!</h1>
---
+++ src/Menu.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+ const Menu = () => {
+    return (
+        <div>
+            <h1>Menu</h1>
+            <p> Feed me feed me</p>
+        </div>
+    )
+}
+export default Menu;
